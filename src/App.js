273fa931f0f824ed9e759af02eb46dc1c1c25138
TextError <savinraul1983@yahoo.com>
2019-08-06 16:50:44 +0100

    first commit

--- src/App.js
+++ src/App.js
@@ -1,26 +1,19 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { Component } from 'react';
+
+// Components
+import MonsterInc from './components/Monster_Inc';
+
+// Css
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+  render() {
+    return (
+      <div>
+        <MonsterInc />
+      </div>
+    )
+  }
 }
 
 export default App;
---
+++ src/components/Monster_Inc.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react'
+
+export default class MonsterInc extends Component {
+  render() {
+    return (
+      <div>
+        monster
+      </div>
+    )
+  }
+}
