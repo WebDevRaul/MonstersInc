1c70c619c9a3eaa58e7d59f86c835d08a530682e
TextError <savinraul1983@yahoo.com>
2019-08-06 17:46:59 +0100

    monster robohash

--- src/components/Monster.js
+++ src/components/Monster.js
@@ -1,9 +1,11 @@
 import React from 'react';
 
-export const Monster = ({ name }) => {
+export const Monster = ({ name, id, email }) => {
   return(
-    <div>
+    <div className='card-container'>
+      <img alt='monster' src={`https://robohash.org/${id}?set=set2`} />
       <h1>{name}</h1>
+      <h2>{email}</h2>
     </div>
   )
 }
--- src/components/Monster_Card.js
+++ src/components/Monster_Card.js
@@ -6,8 +6,8 @@ import { Monster } from './Monster';
 export const MonsterCard = ({ monsters }) => {
   return(
     <div className='card-list'>
-      {monsters.map(({ id, name }) => (
-        <Monster key={id} name={name} />
+      {monsters.map(({ id, name, email }) => (
+        <Monster key={id} name={name} id={id} email={email} />
       ))}
     </div>
   )
