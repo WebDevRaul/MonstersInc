5187d3a89fb89dbe9272de6368a05cc88a0ac094
TextError <savinraul1983@yahoo.com>
2019-08-08 17:56:07 +0100

    prepare for heroku & make more responsive

--- src/components/Monster.js
+++ src/components/Monster.js
@@ -1,13 +1,19 @@
 import React from 'react';
+import PropTypes from 'prop-types';
 
 export const Monster = ({ name, email }) => {
   return(
-    <div className='col col-sm-6 col-md-4 p-3'>
+    <div className='col-10 col-sm-6 col-md-4 p-3 m-auto'>
       <div className='robo-container'>
-        <img alt='monster' src={`https://robohash.org/${name}`} />
-        <h3>{name}</h3>
+        <img alt='monster' className='image' src={`https://robohash.org/${name}`} />
+        <p className='text-uppercase font-weight-bold pt-2'>{name}</p>
         <p>{email}</p>
       </div>
     </div>
   )
+};
+
+Monster.propTypes = {
+  name: PropTypes.string.isRequired,
+  email: PropTypes.string.isRequired
 }
--- src/components/Monster_Card.js
+++ src/components/Monster_Card.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import PropTypes from 'prop-types';
 
 // Components
 import { Monster } from './Monster';
@@ -6,9 +7,13 @@ import { Monster } from './Monster';
 export const MonsterCard = ({ monsters }) => {
   return(
     <div className='row no-gutters'>
-      {monsters.map(({ id, name, email }) => (
-        <Monster key={id} name={name} email={email} />
+      {monsters.map(({ id, ...rest }) => (
+        <Monster key={id} { ...rest } />
       ))}
     </div>
   )
+}
+
+MonsterCard.propTypes = {
+  monsters: PropTypes.array.isRequired
 }
